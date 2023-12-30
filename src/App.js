bee7bb0c2974afc1d9efdab69df326758e969407
china raju <chinaraju8877@gmail.com>
2023-12-30 09:25:11 +0530

    second

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './Home/Home';
+import Registration from './Home/Registration';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <header >
        <BrowserRouter>
        <Routes>
-        <Route path="/" element={<Home></Home>}/>
+        {/* <Route path="/" element={<Home></Home>}/> */}
+        <Route path="/" element={<Registration></Registration>}/>
        </Routes>
        </BrowserRouter>
       </header>
--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import Font Awesome CSS
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
